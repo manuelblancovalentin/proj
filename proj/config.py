99f37748887e322feb807713e7a53f231606f40f
Manu <mbvalentin@spaceshipair24.taileb3522.ts.net>
2026-09-24 13:25:13 -0500

    Compatibility with old tomli for python3.9-

--- proj/config.py
+++ proj/config.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
 import os
-import tomllib
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
 from dataclasses import dataclass
 from pathlib import Path
 
--- proj/editor.py
+++ proj/editor.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
 import json
-import tomllib
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
 from pathlib import Path
 from typing import Any
 
